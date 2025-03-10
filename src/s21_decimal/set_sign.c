1e4ef70f9d3d103d8f09210f4a7bda5b50e7117a
onweg <innokentiydiscord@yandex.ru>
2025-03-10 15:24:42 +0300

    do seet_sign.c

--- src/s21_decimal/set_sign.c
+++ src/s21_decimal/set_sign.c
@@ -1,6 +1,11 @@
 #include "../s21_decimal.h"
 
 void set_sign(s21_decimal *value, int sign){
-    return;
+    if (sign == 0) {
+		value->bits[3] &= ~(1 << S21_DEC_SIGN_SHIFT);
+	} else if (sign == 1) {
+		value->bits[3] |= (1 << S21_DEC_SIGN_SHIFT);
+	}
+	return;
 }
 
