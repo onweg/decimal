fcd2099ae09aa87bf4fc1bfdcf30eb34690ea4ff
onweg <innokentiydiscord@yandex.ru>
2025-03-10 15:35:57 +0300

    add macros SIGN_SHIFT

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -11,6 +11,7 @@ typedef struct s21_decimal{
 } s21_decimal;
 
 #define S21_DEC_SIGN_MASK       0b10000000000000000000000000000000
+#define S21_DEC_SIGN_SHIFT      31
 #define S21_DEC_EXP_MASK        0b00000000111111110000000000000000
 #define S21_DEC_EXP_SHIFT       16
 #define S21_DEC_SCALE_MAX       28      
