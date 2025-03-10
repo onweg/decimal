c64970389a8b3a0621cfc726dfb6edde27660cdf
onweg <innokentiydiscord@yandex.ru>
2025-03-10 15:12:46 +0300

    do set_bit.c

--- src/s21_decimal/set_bit.c
+++ src/s21_decimal/set_bit.c
@@ -1,6 +1,16 @@
 #include "../s21_decimal.h"
 
 void set_bit(s21_decimal *value, int i, int bit){
+    int shift, bit_cell;
+    if (i >= 0 && i < 96) {
+        shift = i % 32;
+        bit_cell = i / 32;
+        if (bit == 0) {
+            value->bits[bit_cell] &= ~(1 << shift);
+        } else if (bit == 1) {
+            value->bits[bit_cell] |= (1 << shift);
+        }
+    }
     return;
 }
 
