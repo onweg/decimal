d27571676e0bff2901a86077908699807c5690e5
onweg <innokentiydiscord@yandex.ru>
2025-03-11 15:00:08 +0300

    change shift_bits_to_left.c

--- src/s21_decimal/shift_bits_to_left.c
+++ src/s21_decimal/shift_bits_to_left.c
@@ -1,6 +1,13 @@
 #include "../s21_decimal.h"
 
 void shift_bits_to_left(s21_decimal *value, int shift) {
+	if (shift <= 0) return; 
+	if (shift > 95) {
+		value->bits[0] = 0;
+		value->bits[1] = 0;
+		value->bits[2] = 0;
+		return;
+	}
     for (int i = 95; i >= shift; i--) {
 		set_bit(value, i, get_bit(*value, i - shift));
 	}
