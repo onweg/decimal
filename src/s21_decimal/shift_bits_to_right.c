aa8b56a904a9b955596fedc1ba7f46df6d449bf2
onweg <innokentiydiscord@yandex.ru>
2025-03-11 15:05:49 +0300

    do shift_bits_to_right

---
+++ src/s21_decimal/shift_bits_to_right.c
@@ -0,0 +1,17 @@
+#include "../s21_decimal.h"
+
+void shift_bits_to_right(s21_decimal *value, int shift) {
+	if (shift <= 0) return; 
+	if (shift > 95) {
+		value->bits[0] = 0;
+		value->bits[1] = 0;
+		value->bits[2] = 0;
+		return;
+	}
+    for (int i = 0; i < 96 - shift; i++) {
+		set_bit(value, i, get_bit(*value, i + shift));
+	}
+	set_bit(value, 95, 0);
+}
+
+
