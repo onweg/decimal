30079d6dbcef54d88f9295293aa5803fed7f3702
onweg <innokentiydiscord@yandex.ru>
2025-03-14 20:17:24 +0300

    do scale_increase.c

--- src/s21_decimal/scale_increase.c
+++ src/s21_decimal/scale_increase.c
@@ -7,20 +7,21 @@ int scale_increase(s21_decimal *value, int shift) {
 	if (shift <= 0) { return 0; }
 	if (get_exp(*value) + shift > 28) { return 1; }
 	for (int i = 0; i < shift; i++) {
-		s21_decimal tmp1 = {0};
-		copy(&tmp1, *value);
-		s21_decimal tmp2 = {0};
-		copy(&tmp2, *value);
-		s21_decimal tmp_result = {0};
-		copy(&tmp_result, *value);
+		s21_decimal tmp1 = *value;
+		s21_decimal tmp2 = *value;
+		s21_decimal tmp_result = *value;
 		
 		shift_bits_to_left(&tmp1, 1);
 		shift_bits_to_left(&tmp2, 3);
+
 		if (calculating_sum_mantissa(tmp1, tmp2, &tmp_result) == 1) {
 			// переполнение
 			return 1;
 		}
-		copy(value, tmp_result);
+		
+		value->bits[0] = tmp_result.bits[0];
+		value->bits[1] = tmp_result.bits[1];
+		value->bits[2] = tmp_result.bits[2];
 		set_exp(value, get_exp(*value) + 1);
 	}
 	return 0;
