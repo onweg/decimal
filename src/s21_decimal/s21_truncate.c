cf760219564fefcf02def4f68f1e207bd615fe68
onweg <innokentiydiscord@yandex.ru>
2025-03-22 15:10:02 +0300

    add s21_truncate.c

--- src/s21_decimal/s21_truncate.c
+++ src/s21_decimal/s21_truncate.c
@@ -1,6 +1,13 @@
 #include "../s21_decimal.h"
 
-int s21_truncate(s21_decimal value, s21_decimal *result){
+int s21_truncate(s21_decimal value, s21_decimal *result) {
+    if (result == NULL) { return 1; }
+    int exp = s21_get_exp(value);
+    while (exp > 0) {
+        s21_scale_decrease(&value, 1);
+        exp--;
+    }
+    s21_copy(result, value);
     return 0;
 }
 
